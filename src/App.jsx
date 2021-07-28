a02731f7868739d3a662c85d1b6fc96cc253b074
Michael <mike.meidenbauer@gmail.com>
2021-07-27 20:37:31 -0500

    add a few more things

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Login />
         <h1>MICHAEL AND LUCAS RULE</h1>
       </header>
     </div>
---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => (
+  <h1>
+    Fake Login Header
+  </h1>
+);
+
+export default Login;
