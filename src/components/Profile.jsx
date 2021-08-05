d4f119c6d0156263333daa85ea3c9c6f9cb2723a
Lucas Wakefield <lucas.wakefield@gmial.com>
2021-08-04 23:08:36 -0500

    Links to posts work. Running into a strange bug in the Message from user section when checking to see if the original post is still active.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,40 +1,69 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from "react";
-import { getMyInfo } from "../api/api";
+import { Link } from "react-router-dom"; 
+import { getMyInfo, getAllPosts } from "../api/api";
 
 const Profile = () => {
   const [myInfo, updateMyInfo] = useState({});
+  const [allPosts, updateAllPosts] = useState([]);
+
   useEffect(async () => {
     const token = JSON.parse(
       window.localStorage.getItem("strangersThingsToken")
     );
     const myInfoResponse = await getMyInfo(token);
+    const myPostsResponse = await getAllPosts(token);
     updateMyInfo(myInfoResponse.data);
+    updateAllPosts(myPostsResponse);
   }, []);
-  console.log(myInfo);
+  console.log(allPosts);
   return (
     <div>
       <h2>Messages to {myInfo.username}</h2>
-      {/* need logic to match id and only render my messages */}
       {myInfo.messages &&
         myInfo.messages
           .filter((message) => message.fromUser.username !== myInfo.username)
-          .map((message) => (
+          .map((message) => {
+            const postId = message.post._id;
+            console.log(postId);
+            const matchedPost = myInfo.posts.filter(post => post._id === postId)[0];
+            console.log(matchedPost);
+            if(matchedPost.active) {
+            return (
             <div className="message">
               <h3>From: {message.fromUser.username}</h3>
               <p>{message.content}</p>
-              <p>Post: {message.post.title}</p>
+              <Link to={`/posts/${postId}`} ><p>Post: {message.post.title}</p> </Link>
             </div>
-          ))}
+          )} return (
+              <div className="message">
+              <h3>From: {message.fromUser.username}</h3>
+              <p>{message.content}</p>
+              <p>Post: {message.post.title}</p>
+              <p>(deleted post)</p>
+            </div>
+            )
+          })}
       <h2>Message from {myInfo.username}</h2>
       {myInfo.messages &&
         myInfo.messages
           .filter((message) => message.fromUser.username === myInfo.username)
           .map((message) => (
+            // console.log(message.post._id);
+            // const postId = message.post._id;
+            // console.log(postId);
+            // const matchedPost = allPosts.filter(post => post._id === postId)[0];
+            // console.log(matchedPost);
+            // console.log(allPosts);
+            // console.log(allPosts.find(message.post._id));
+
+             
             <div className="message">
               <p>{message.content}</p>
-              <p>Post: {message.post.title}</p>
+              <Link to={`/posts/${message.post._id}`} ><p>Post: {message.post.title}</p> </Link>
             </div>
-          ))}
+          )
+          )}
     </div>
   );
 };
