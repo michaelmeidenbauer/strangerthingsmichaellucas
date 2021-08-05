c8e9fa09b4372e42944347d0b64bc2f495343a0f
Lucas Wakefield <lucas.wakefield@gmial.com>
2021-08-05 17:39:52 -0500

    matched post check

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import { getMyInfo, getAllPosts } from "../api/api";
 
 const Profile = () => {
@@ -16,7 +16,9 @@ const Profile = () => {
     updateMyInfo(myInfoResponse.data);
     updateAllPosts(myPostsResponse);
   }, []);
+
   console.log(allPosts);
+
   return (
     <div>
       <h2>Messages to {myInfo.username}</h2>
@@ -26,46 +28,70 @@ const Profile = () => {
           .map((message) => {
             const postId = message.post._id;
             console.log(postId);
-            const matchedPost = myInfo.posts.filter(post => post._id === postId)[0];
+            const matchedPost = myInfo.posts.filter(
+              (post) => post._id === postId
+            )[0];
             console.log(matchedPost);
-            if(matchedPost.active) {
+            if (matchedPost && matchedPost.active) {
+              return (
+                <div className="message">
+                  <h3>From: {message.fromUser.username}</h3>
+                  <p>{message.content}</p>
+                  <Link to={`/posts/${postId}`}>
+                    <p>Post: {message.post.title}</p>{" "}
+                  </Link>
+                </div>
+              );
+            }
             return (
-            <div className="message">
-              <h3>From: {message.fromUser.username}</h3>
-              <p>{message.content}</p>
-              <Link to={`/posts/${postId}`} ><p>Post: {message.post.title}</p> </Link>
-            </div>
-          )} return (
               <div className="message">
-              <h3>From: {message.fromUser.username}</h3>
-              <p>{message.content}</p>
-              <p>Post: {message.post.title}</p>
-              <p>(deleted post)</p>
-            </div>
-            )
+                <h3>From: {message.fromUser.username}</h3>
+                <p>{message.content}</p>
+                <p>Post: {message.post.title}</p>
+                <p>(deleted post)</p>
+              </div>
+            );
           })}
       <h2>Message from {myInfo.username}</h2>
       {myInfo.messages &&
         myInfo.messages
           .filter((message) => message.fromUser.username === myInfo.username)
-          .map((message) => (
-            // console.log(message.post._id);
-            // const postId = message.post._id;
-            // console.log(postId);
-            // const matchedPost = allPosts.filter(post => post._id === postId)[0];
-            // console.log(matchedPost);
-            // console.log(allPosts);
-            // console.log(allPosts.find(message.post._id));
-
-             
-            <div className="message">
-              <p>{message.content}</p>
-              <Link to={`/posts/${message.post._id}`} ><p>Post: {message.post.title}</p> </Link>
-            </div>
-          )
-          )}
+          .map((message) => {
+            const postId = message.post._id;
+            console.log(postId);
+            const matchedPost = allPosts.filter(
+              (post) => post._id === postId
+            )[0];
+            console.log(matchedPost);
+            if (matchedPost && matchedPost.active) {
+              return (
+                <div className="message">
+                  <h3>From: {message.fromUser.username}</h3>
+                  <p>{message.content}</p>
+                  <Link to={`/posts/${postId}`}>
+                    <p>Post: {message.post.title}</p>{" "}
+                  </Link>
+                </div>
+              );
+            }
+            return (
+              <div className="message">
+                <h3>From: {message.fromUser.username}</h3>
+                <p>{message.content}</p>
+                <p>Post: {message.post.title}</p>
+                <p>(deleted post)</p>
+              </div>
+            );
+          })}
     </div>
   );
 };
+
+//           <div className="message">
+//             <p>{message.content}</p>
+//             <Link to={`/posts/${message.post._id}`} ><p>Post: {message.post.title}</p> </Link>
+//           </div>
+//         )
+//         )
 
 export default Profile;
