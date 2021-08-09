abcc1a17b718ccdd7365caa5f920675a6e49d50d
Michael <mike.meidenbauer@gmail.com>
2021-08-09 01:14:21 -0500

    add name in navigation
    add name in navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import Login from './components/Login';
@@ -8,16 +8,36 @@ import SinglePost from './components/SinglePost';
 import Postform from './components/Postform';
 import EditPost from './components/EditPost';
 import Navigation from './components/Navigation';
+import { getMyInfo } from './api/api';
 import BACFullsize from './images/BAC-fullsize.png';
 
 
 function App() {
   const [isLoggedIn, updateIsLoggedIn] = useState(false);
+  const [loggedInName, updateLoggedInName] = useState(null);
+
+  useEffect(
+    async () => {
+      const localToken = JSON.parse(localStorage.getItem("strangersThingsToken"));
+      if (localToken) {
+        updateIsLoggedIn(true);
+        const userInfo = await getMyInfo(localToken);
+        const userName = userInfo.data.username;
+        updateLoggedInName(userName);
+      }
+    },
+    []
+  );
+  // eslint-disable-next-line no-unused-vars
 
   return (
     <BrowserRouter>
       <div className="App">
-        <Navigation isLoggedIn={isLoggedIn} updateIsLoggedIn={updateIsLoggedIn}/>
+        <Navigation
+        isLoggedIn={isLoggedIn}
+        updateIsLoggedIn={updateIsLoggedIn}
+        loggedInName={loggedInName}
+        updateLoggedInName={updateLoggedInName}/>
         <Switch>
           <Route exact path="/posts" component={Posts} />
           <Route exact path="/posts/add" component={Postform} />
@@ -29,7 +49,10 @@ function App() {
           </Route>
 
           <Route path="/login">
-            <Login updateIsLoggedIn={updateIsLoggedIn}/>
+            <Login
+            updateIsLoggedIn={updateIsLoggedIn}
+            updateLoggedInName={updateLoggedInName}
+            />
           </Route>
           <Route path="/">
             <header className="App-header">
